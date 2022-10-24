00595fce51e62e36be52c5fbcc33211d563f9a4a
mshepelev <mshepelev@gid.ru>
2022-10-24 19:46:50 +0300

    added style and js linters && added engine.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 window.stateOfGame = {
-	level: null,
-	screen: 'start',
-	time: 0,
-	cards: null,
-	selectedCards: null
+  level: null,
+  screen: 'start',
+  time: 0,
+  cards: null,
+  selectedCards: null,
 }
 
-function init(){
-	function startGame(event, form){
-		event.preventDefault();
+function init() {
+  function startGame(event, form) {
+    event.preventDefault()
 
-		const formData = new FormData(form)
+    const formData = new FormData(form)
 
-		const level = formData.get('level')
+    const level = formData.get('level')
 
-		if(!level){
-			alert('Выбери сложность!')
+    if (!level) {
+      alert('Выбери сложность!')
 
-			return
-		}
+      return
+    }
 
-		alert(`выбраный уровень ${level}`)
+    alert(`выбраный уровень ${level}`)
 
-		window.stateOfGame.level = level
-	}
+    window.stateOfGame.level = level
+  }
 
-	function initGame(){
-		const form = document.querySelector('.select-level__form')
-		form.addEventListener('submit', (event) => startGame(event, form))
-	}
+  function initGame() {
+    const form = document.querySelector('.select-level__form')
+    form.addEventListener('submit', (event) => startGame(event, form))
+  }
 
-	initGame()
+  initGame()
 }
 
 window.addEventListener('DOMContentLoaded', init)
